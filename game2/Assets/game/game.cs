2df3878774e43210e3fcb81f4929d7be4242f192
Mounika1029 <mounikak793@gmail.com>
2025-09-26 15:41:40 +0530

    project added

---
+++ game2/Assets/game/game.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+
+    [SerializeField] private float movespeed;
+    private void Update()
+    {
+        movement();
+    }
+    void movement()
+    {
+        float xvalue = Input.GetAxis("Horizontal") * movespeed* Time.deltaTime;
+        float zvalue = Input.GetAxis("Vertical") * movespeed* Time.deltaTime;
+        transform.Translate(xvalue, 0, zvalue);
+
+
+    }
+}
+
