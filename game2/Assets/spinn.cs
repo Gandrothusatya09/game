34bffad781751252e7e47dea28fcc329a3518900
Mounika1029 <satyagandrothu09@gmail.com>
2025-09-27 13:34:34 +0530

    Added Materials

---
+++ game2/Assets/spinn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        transform.Rotate(0, 0.1f, 0);
+    }
+}
