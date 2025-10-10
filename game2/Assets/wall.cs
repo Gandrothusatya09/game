ea080749cb83b63dbe09d75da06fd1c005e9b207
Mounika1029 <satyagandrothu09@gmail.com>
2025-10-10 14:43:09 +0530

    color added

--- game2/Assets/wall.cs
+++ game2/Assets/wall.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class wall : MonoBehaviour
 {
     public MeshRenderer m_Renderer;
-    [SerializeField] private GameObject droppy;
+    
     private Rigidbody rb;
+
+    
     private void Start()
     {
         m_Renderer = GetComponent<MeshRenderer>();
 
-        rb = droppy.GetComponent<Rigidbody>();
-        rb.useGravity = false;
+       
     }
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Dummy")
+        if (other.gameObject.tag == "Player")
         {
             m_Renderer.material.color = Color.red;
-
+           
         }
     }
     private void OnCollisionExit(Collision other)
     {
-        if (other.gameObject.tag == "Dummy")
+        if (other.gameObject.tag == "Player")
         {
             m_Renderer.material.color = Color.blue;
         }
